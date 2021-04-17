3f70610939b1a3e4e9a42d107ff0fb4bf15fe9d7
Frenz86 <frenzy86@gmail.com>
2021-04-17 22:38:49 +0200

    ww

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, HTTPException, Request
-from pydantic import BaseModel
+from fastapi.responses import HTMLResponse, RedirectResponse,JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
-import joblib
+from pydantic import BaseModel
+from typing import Optional, List
+
 import numpy as np
 import pandas as pd
-from fastapi.responses import HTMLResponse, RedirectResponse,JSONResponse
 import json
-from typing import Optional, List
+import joblib
 
 # Variabili globali
 loaded_model = None
@@ -42,26 +43,37 @@ async def startup_event():
     # caricamento modelli
     # load all stuff
     global loaded_model
-    loaded_model = joblib.load('LRClassifier.pkl')
+    loaded_model = joblib.load('BostonClassifier.pkl')
 
 async def shutdon_envent():
     print("Shutdown")
 
 class InputModel(BaseModel):
-    sepal_length: float
-    sepal_width: float
-    petal_length: float
-    petal_width: float
+    input_1: float
+    input_2: float
+    input_3: float
+    input_4: float
+    input_5: float
+    input_6: float
+    input_7: float
+    input_8: float
+    input_9: float
+    input_10: float
+    input_11: float
+    input_12: float
+    input_13: float
     startDatetime:  Optional[float] =  None
 
 @app.post('/predict',response_class=JSONResponse )
-def predict_species(iris: InputModel):
-    data = iris.dict()
-    #print(data)
-    data_in = [[data['sepal_length'], data['sepal_width'], data['petal_length'], data['petal_width']]]
+def predict_species(input_: InputModel):
+    data = input_.dict()
+    data_in = [[data['input_1'], data['input_2'], data['input_3'], data['input_4'],
+                data['input_5'], data['input_6'], data['input_7'], data['input_8'],
+                data['input_9'], data['input_10'], data['input_11'], data['input_12'],data['input_13']]]
+    
     prediction = loaded_model.predict(data_in)
-    probability = loaded_model.predict_proba(data_in).max()
+    #probability = loaded_model.predict_proba(data_in).max()
     return json.dumps({
                         'prediction': prediction[0],
-                        'probability': probability
+                        #'probability': probability
                         })
